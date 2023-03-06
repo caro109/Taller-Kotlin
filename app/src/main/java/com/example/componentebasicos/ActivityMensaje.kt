1feeacbc43b25888d38dd89ced65527f923b0053
mateo208 <mateo20818@gmail.com>
2023-03-06 17:06:17 -0500

    agregando proyecto

---
+++ app/src/main/java/com/example/componentebasicos/ActivityMensaje.kt
@@ -0,0 +1,11 @@
+package com.example.componentebasicos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMensaje : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensaje)
+    }
+}
