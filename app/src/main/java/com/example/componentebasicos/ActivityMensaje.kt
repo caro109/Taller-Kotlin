351404a76bb61415e9a697372c466863ad9950b4
caro109 <carogarcia.0419@gmail.com>
2023-03-09 17:48:14 -0500

    Se agrega cambios

--- app/src/main/java/com/example/componentebasicos/ActivityMensaje.kt
+++ app/src/main/java/com/example/componentebasicos/ActivityMensaje.kt
@@ -3,6 +3,7 @@ package com.example.componentebasicos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import android.widget.Toast
 
 class ActivityMensaje : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +17,22 @@ class ActivityMensaje : AppCompatActivity() {
     private fun Imprimir(){
 
         val editText = findViewById<EditText>(R.id.editTextTextPersonName)
-        val editTextValue = editText.text.toString()
         val radioGroup = findViewById<RadioGroup>(R.id.idRadioGroup)
         val checkBox1 = findViewById<CheckBox>(R.id.checkBox)
         val checkBox2 = findViewById<CheckBox>(R.id.checkBox2)
         val togglebuton = findViewById<ToggleButton>(R.id.toggleButton)
         val switch = findViewById<Switch>(R.id.switch1)
 
-        Toast.makeText(this, "$editTextValue , $radioGroup seleccionado, $checkBox1 seleccionado, $checkBox2 seleccionado, $togglebuton Seleccionado, $switch ", Toast.LENGTH_LONG).show()
+
+        val texto = "Valor de EditText: ${editText.text}\n" +
+                "Valor de RadioGroup: ${radioGroup.checkedRadioButtonId}\n" +
+                "Valor de CheckBox1: ${checkBox1.isChecked}\n" +
+                "Valor de CheckBox2: ${checkBox2.isChecked}\n" +
+                "Valor de ToggleButton: ${togglebuton.isChecked}\n" +
+                "Valor de Switch: ${switch.isChecked}"
+
+
+
+        Toast.makeText(this, "$texto ", Toast.LENGTH_LONG).show()
     }
 }
